59de8f7c7f96d65ccb274d481ad24e81c824cb04
sepandhaghighi <sepand.haghighi@yahoo.com>
2023-07-11 21:26:43 +0430

    fix : TONES_MAP updated

--- mytimer/params.py
+++ mytimer/params.py
@@ -10,7 +10,16 @@ MESSAGE_TEMPLATE = "Message: {0}"
 
 TONES_MAP = {
     1: '1.wav',
-    2: '2.wav'}
+    2: '2.wav',
+    3: '3.wav',
+    4: '4.wav',
+    5: '5.wav',
+    6: '6.wav',
+    7: '7.wav',
+    8: '8.wav',
+    9: '9.wav',
+    10: '10.wav',
+    11: '11.wav'}
 
 FACES_MAP = {
     1: 'bulbhead',
