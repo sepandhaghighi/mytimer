174a3114421f94b4f4bdcecbc5a8ccdfd51c4242
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-11-28 14:00:40 +0330

    doc : MY_TIMER_OVERVIEW updated

--- mytimer/params.py
+++ mytimer/params.py
@@ -21,11 +21,10 @@ KEEP_ON_MAX = 10000000
 SIGNS_LIST = ["", "+", "-"]
 
 MY_TIMER_OVERVIEW = '''
-MyTimer is a Python project that aims to provide a simple yet efficient timer for terminal users, particularly targeting the geek community.
-This project allows users to set timers directly from their command line interface, making it convenient for those who spend a significant amount of time working in the terminal!
-
-The main objective of MyTimer is to offer a minimalistic and distraction-free timer experience.
-It provides a clean and straightforward interface, ensuring that users can focus solely on tracking time without any unnecessary clutter or distractions.
+MyTimer is a Python project that aims to provide a simple yet efficient timer for terminal users,
+particularly targeting the geek community.
+This project allows users to set timers directly from their command line interface,
+making it convenient for those who spend a significant amount of time working in the terminal!
 '''
 
 MY_TIMER_REPO = "Repo : https://github.com/sepandhaghighi/mytimer"
