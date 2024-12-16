b30794eeff0bf63304c1ad82d5ae1350083dd31a
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-12-16 14:51:15 +0330

    rel : migrate to version 1.9

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read_description():
 setup(
     name='mytimer',
     packages=['mytimer'],
-    version='1.8',
+    version='1.9',
     description='A Geeky Timer for Terminal Enthusiasts',
     long_description=read_description(),
     long_description_content_type='text/markdown',
@@ -37,7 +37,7 @@ setup(
     author='Sepand Haghighi',
     author_email='me@sepand.tech',
     url='https://github.com/sepandhaghighi/mytimer',
-    download_url='https://github.com/sepandhaghighi/mytimer/tarball/v1.8',
+    download_url='https://github.com/sepandhaghighi/mytimer/tarball/v1.9',
     keywords="timer time clock terminal stopwatch cli geek",
     project_urls={
         'Source': 'https://github.com/sepandhaghighi/mytimer'
