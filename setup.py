6ce0479acc5b3c68afd8a67e1367932bd32c65ee
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-04-12 09:27:31 +0330

    `Ubuntu-20.04` --> `Ubuntu-22.04` (#155)
    * fix : Ubuntu-20.04 --> Ubuntu-22.04
    
    * fix : setup.py updated
    
    * doc : CHANGELOG.md updated

--- setup.py
+++ setup.py
@@ -44,13 +44,12 @@ setup(
         'Source': 'https://github.com/sepandhaghighi/mytimer'
     },
     install_requires=get_requires(),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
