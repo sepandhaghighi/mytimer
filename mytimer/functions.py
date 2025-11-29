311e88b16b9bcb413436dddd486a57bc7451cb76
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-11-29 13:20:09 +0330

    fix : minor edit in set_color function

--- mytimer/functions.py
+++ mytimer/functions.py
@@ -239,7 +239,7 @@ def set_color(color: Optional[str]) -> None:
     """
     if color:
         color = color.strip().upper()
-        print(getattr(Fore, color, Fore.RESET))
+        print(getattr(Fore, color, ""), end="")
 
 
 def get_tone(index: int) -> str:
