fc493985f6ffd8bf9e8b098d47ccff8fd3bbc222
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-12-14 17:02:51 +0330

    fix : arguments moved to handle_args function

--- mytimer/__main__.py
+++ mytimer/__main__.py
@@ -1,87 +1,12 @@
 # -*- coding: utf-8 -*-
 """mytimer main."""
-from mytimer.params import FACES_LIST, PROGRAMS_MAP, TONES_LIST
-from mytimer.params import EXIT_MESSAGE, ADDITIONAL_INFO, SIGNS_LIST
-from mytimer.params import DATE_SYSTEMS_LIST, COLORS_LIST, INTENSITY_LIST
-from mytimer.functions import run_timer
-import argparse
+from mytimer.params import EXIT_MESSAGE
+from mytimer.functions import handle_args, run_timer
 
 
 def main() -> None:
     """CLI main function."""
-    parser = argparse.ArgumentParser()
-    parser.epilog = ADDITIONAL_INFO
-    parser.add_argument('--minute', help='minute', type=int)
-    parser.add_argument('--second', help='second', type=int)
-    parser.add_argument('--hour', help='hour', type=int)
-    parser.add_argument('--message', help='message', type=str)
-    parser.add_argument(
-        '--face',
-        help='face',
-        type=int,
-        choices=FACES_LIST)
-    parser.add_argument(
-        '--color',
-        help='text color',
-        type=str.lower,
-        choices=COLORS_LIST)
-    parser.add_argument(
-        '--bg-color',
-        help='background color',
-        type=str.lower,
-        choices=COLORS_LIST)
-    parser.add_argument(
-        '--intensity',
-        help='text intensity',
-        type=str.lower,
-        choices=INTENSITY_LIST)
-    parser.add_argument(
-        '--tone',
-        help='alarm tone',
-        type=int,
-        choices=TONES_LIST)
-    parser.add_argument(
-        '--program',
-        help='program',
-        type=str.lower,
-        choices=sorted(PROGRAMS_MAP))
-    parser.add_argument(
-        '--sign',
-        help='timer sign',
-        type=str,
-        choices=SIGNS_LIST)
-    parser.add_argument('--alarm-repeat', help='alarm repeat', type=int)
-    parser.add_argument('--v-shift', help='vertical shift', type=int)
-    parser.add_argument('--h-shift', help='horizontal shift', type=int)
-    parser.add_argument('--repeat', help='number of repeats', type=int, default=1)
-    parser.add_argument(
-        '--countdown',
-        help='countdown timer',
-        nargs="?",
-        const=1)
-    parser.add_argument('--countup', help='countup timer', nargs="?", const=1)
-    parser.add_argument('--alarm', help='alarm', nargs="?", const=1)
-    parser.add_argument('--keep-on', help='keep-on', nargs="?", const=1)
-    parser.add_argument('--set-on', help='set-on', nargs="?", const=1)
-    parser.add_argument('--test-tone', help='test tone', nargs="?", const=1)
-    parser.add_argument('--faces-list', help='faces list', nargs="?", const=1)
-    parser.add_argument('--version', help='version', nargs="?", const=1)
-    parser.add_argument('--info', help='info', nargs="?", const=1)
-    parser.add_argument('--hide-second', help='hide second', nargs="?", const=1)
-    parser.add_argument('--hide-datetime', help='hide datetime', nargs="?", const=1)
-    parser.add_argument('--vertical', help='vertical mode', nargs="?", const=1)
-    parser.add_argument(
-        '--date-system',
-        help='date system',
-        type=str.lower,
-        choices=DATE_SYSTEMS_LIST,
-        default="gregorian")
-    parser.add_argument(
-        '--programs-list',
-        help='programs list',
-        nargs="?",
-        const=1)
-    args = parser.parse_args()
+    args = handle_args()
     try:
         run_timer(args)
     except (KeyboardInterrupt, EOFError):
--- mytimer/functions.py
+++ mytimer/functions.py
@@ -23,6 +23,9 @@ from mytimer.params import MY_TIMER_OVERVIEW, MY_TIMER_REPO
 from mytimer.params import TIME_HMS_TEMPLATE_HORIZONTAL, TIME_HM_TEMPLATE_HORIZONTAL
 from mytimer.params import TIME_HMS_TEMPLATE_VERTICAL, TIME_HM_TEMPLATE_VERTICAL
 from mytimer.params import CLOCK_FORMAT, DATE_FORMAT
+from mytimer.params import FACES_LIST, PROGRAMS_MAP, TONES_LIST
+from mytimer.params import EXIT_MESSAGE, ADDITIONAL_INFO, SIGNS_LIST
+from mytimer.params import DATE_SYSTEMS_LIST, COLORS_LIST, INTENSITY_LIST
 from art import tprint
 
 
@@ -602,6 +605,84 @@ def select_timer_function(args: argparse.Namespace, params: Dict[str, Any]) -> T
             params["hour"] = KEEP_ON_MAX
             timer_function = countup_timer
     return timer_function, params
+
+
+def handle_args() -> None:
+    """Handle CLI arguments."""
+    parser = argparse.ArgumentParser()
+    parser.epilog = ADDITIONAL_INFO
+    parser.add_argument('--minute', help='minute', type=int)
+    parser.add_argument('--second', help='second', type=int)
+    parser.add_argument('--hour', help='hour', type=int)
+    parser.add_argument('--message', help='message', type=str)
+    parser.add_argument(
+        '--face',
+        help='face',
+        type=int,
+        choices=FACES_LIST)
+    parser.add_argument(
+        '--color',
+        help='text color',
+        type=str.lower,
+        choices=COLORS_LIST)
+    parser.add_argument(
+        '--bg-color',
+        help='background color',
+        type=str.lower,
+        choices=COLORS_LIST)
+    parser.add_argument(
+        '--intensity',
+        help='text intensity',
+        type=str.lower,
+        choices=INTENSITY_LIST)
+    parser.add_argument(
+        '--tone',
+        help='alarm tone',
+        type=int,
+        choices=TONES_LIST)
+    parser.add_argument(
+        '--program',
+        help='program',
+        type=str.lower,
+        choices=sorted(PROGRAMS_MAP))
+    parser.add_argument(
+        '--sign',
+        help='timer sign',
+        type=str,
+        choices=SIGNS_LIST)
+    parser.add_argument('--alarm-repeat', help='alarm repeat', type=int)
+    parser.add_argument('--v-shift', help='vertical shift', type=int)
+    parser.add_argument('--h-shift', help='horizontal shift', type=int)
+    parser.add_argument('--repeat', help='number of repeats', type=int, default=1)
+    parser.add_argument(
+        '--countdown',
+        help='countdown timer',
+        nargs="?",
+        const=1)
+    parser.add_argument('--countup', help='countup timer', nargs="?", const=1)
+    parser.add_argument('--alarm', help='alarm', nargs="?", const=1)
+    parser.add_argument('--keep-on', help='keep-on', nargs="?", const=1)
+    parser.add_argument('--set-on', help='set-on', nargs="?", const=1)
+    parser.add_argument('--test-tone', help='test tone', nargs="?", const=1)
+    parser.add_argument('--faces-list', help='faces list', nargs="?", const=1)
+    parser.add_argument('--version', help='version', nargs="?", const=1)
+    parser.add_argument('--info', help='info', nargs="?", const=1)
+    parser.add_argument('--hide-second', help='hide second', nargs="?", const=1)
+    parser.add_argument('--hide-datetime', help='hide datetime', nargs="?", const=1)
+    parser.add_argument('--vertical', help='vertical mode', nargs="?", const=1)
+    parser.add_argument(
+        '--date-system',
+        help='date system',
+        type=str.lower,
+        choices=DATE_SYSTEMS_LIST,
+        default="gregorian")
+    parser.add_argument(
+        '--programs-list',
+        help='programs list',
+        nargs="?",
+        const=1)
+    args = parser.parse_args()
+    return args
 
 
 def run_timer(args: argparse.Namespace) -> None:
