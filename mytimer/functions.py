0a729f40cfc5f5c00beefcca3e546260b673b1de
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-12-20 12:03:43 +0330

    fix : main function moved to functions.py

--- mytimer/functions.py
+++ mytimer/functions.py
@@ -13,7 +13,7 @@ from colorama import Fore, Back, Style
 from mytimer.params import INPUT_ERROR_MESSAGE, SOUND_ERROR_MESSAGE
 from mytimer.params import INPUT_EXAMPLE, TIME_ELEMENTS, MESSAGE_TEMPLATE
 from mytimer.params import FACES_MAP, PROGRAMS_MAP, BREAKS_MAP, TONES_MAP
-from mytimer.params import MY_TIMER_VERSION, PROGRAMS_LIST_TEMPLATE
+from mytimer.params import MY_TIMER_VERSION, PROGRAMS_LIST_TEMPLATE, EXIT_MESSAGE
 from mytimer.params import FACES_LIST_EXAMPLE_MESSAGE
 from mytimer.params import DEFAULT_PARAMS, PROGRAMS_DEFAULTS, BREAKS_DEFAULTS
 from mytimer.params import NEXT_PROGRAM_MESSAGE, END_ROUND_MESSAGE
@@ -719,3 +719,41 @@ def run_timer(timer_function: Callable, params: Dict[str, dict], repeat: int, pr
         timer_round += 1
     if keep_on:
         keep_on_timer(params["timer"])
+
+
+def main() -> None:
+    """CLI main function."""
+    args = handle_args()
+    set_color(color=args.color)
+    set_bg_color(bg_color=args.bg_color)
+    set_intensity(intensity=args.intensity)
+    params = load_params(args)
+    timer_function, params = select_timer_function(args, params)
+    if args.set_on:
+        params = update_set_on_params(params)
+    if args.version:
+        print(MY_TIMER_VERSION)
+    elif args.info:
+        print_mytimer_info()
+    elif args.faces_list:
+        show_faces_list()
+    elif args.programs_list:
+        show_programs_list()
+    elif args.test_tone:
+        test_tone(params["tone"], params["alarm_repeat"])
+    else:
+        params_dict = {"timer": params}
+        if args.program == "pomodoro":
+            params_dict["short_break"] = load_params(args, program="pomodoro-short-break", is_break=True)
+            params_dict["long_break"] = load_params(args, program="pomodoro-long-break", is_break=True)
+        elif args.program in ["52-17", "112-26", "animedoro"]:
+            params_dict["break"] = load_params(args, is_break=True)
+        try:
+            run_timer(
+                timer_function=timer_function,
+                params=params_dict,
+                repeat=args.repeat,
+                program=args.program,
+                keep_on=args.keep_on)
+        except (KeyboardInterrupt, EOFError):
+            print(EXIT_MESSAGE)
